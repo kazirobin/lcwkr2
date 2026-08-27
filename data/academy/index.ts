0ef7eee4e9656d304e0df08047e10a3bb9539b17
kazirobinking <kazirobinking@gmail.com>
2026-08-27 12:01:42 +0600

    dynamic attend

--- data/academy/index.ts
+++ data/academy/index.ts
@@ -1,7 +1,7 @@
 import { IAcademyData } from "@/types/academy";
-import { studentsData } from "./academy/students";
-import { hsk101Course } from "./academy/hsk101";
-import { hsk201Course } from "./academy/hsk201";
+import { studentsData } from "./students";
+import { hsk101Course } from "./hsk101";
+import { hsk201Course } from "./hsk201";
 
 export const academyData: IAcademyData = {
   institution: "Global Academy",
